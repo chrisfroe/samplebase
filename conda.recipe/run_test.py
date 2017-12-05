2d410229e7ce315e9139dfe79ba860a9242b845c
chrisfroe <christoph.froehner@fu-berlin.de>
2017-12-05 22:53:45 +0100

    restore simpler run_test

--- conda.recipe/run_test.py
+++ conda.recipe/run_test.py
@@ -4,29 +4,8 @@ import nose
 
 import samplebase
 
-
-def run_nose(verbose=False):
-    initial_dir = os.getcwd()
-    samplebase_file = os.path.abspath(samplebase.__file__)
-    samplebase_dir = os.path.dirname(samplebase_file)
-    nose_argv = sys.argv
-    nose_argv += ['--detailed-errors', '--exe']
-    if verbose:
-        nose_argv.append('-v')
-        print("initial_dir", initial_dir)
-        print("samplebase_file", samplebase_file)
-        print("samplebase_dir", samplebase_dir)
-        print("containing ..", os.listdir(samplebase_dir))
-    os.chdir(samplebase_dir)
-    try:
-        exit_status = nose.run(argv=nose_argv)
-    finally:
-        os.chdir(initial_dir)
-    return exit_status
-
-
 if __name__ == '__main__':
-    print("run_test .. ")
-    exit_status = run_nose(verbose=True)
+    target_dir = os.path.dirname(samplebase.__file__)
+    exit_status = nose.run(argv=[sys.argv[0], target_dir, '-v'])
     print("nose returned exit_status: ", exit_status)
     sys.exit(0 if exit_status else 1)

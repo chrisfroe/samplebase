ec54021cc0cf90d5cb28c91daf08a769e88ee6a7
chrisfroe <christoph.froehner@fu-berlin.de>
2017-12-05 21:29:36 +0100

    travis

--- conda.recipe/run_test.py
+++ conda.recipe/run_test.py
@@ -6,6 +6,7 @@ import samplebase
 
 if __name__ == '__main__':
     target_dir = os.path.dirname(samplebase.__file__)
+    print("target dir ", target_dir)
     exit_status = nose.run(argv=[sys.argv[0], target_dir , '-v'])
     print("nose returned exit_status: ", exit_status)
     sys.exit(0 if exit_status else 1)

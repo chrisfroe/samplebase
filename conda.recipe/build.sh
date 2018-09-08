787a0da95fc9eddda7e6c1bc8fa8c2604b0c8d3a
Christoph Fröhner <christoph.froehner@posteo.net>
2018-09-08 22:28:44 +0200

    propagate version to source code

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 unset MACOSX_DEPLOYMENT_TARGET
+
+# set version within the source code
+echo "__version__ = '${PKG_VERSION}_${PKG_BUILDNUM}'" | tee samplebase/version.py
+
 ${PYTHON} setup.py install;
--- samplebase/__init__.py
+++ samplebase/__init__.py
@@ -1,2 +1,4 @@
 from .sample import Sample
 from .samplebase import *
+
+from samplebase.version import __version__ as __version__
